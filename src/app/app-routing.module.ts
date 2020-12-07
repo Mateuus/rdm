9ba0fb82baefef8c20e6b4fdfd0c0d2baf5776d7
Mateuus <mateuus27@outlook.com>
2020-12-06 23:42:32 -0300

    Iniciando Device Lista. Iniciado Map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  /*
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  
+  {
+    path: 'devices',
+    loadChildren: () => import('./devices/devices.module').then( m => m.DevicesPageModule)
+  },
+  {
+    path: 'maps',
+    loadChildren: () => import('./maps/maps.module').then( m => m.MapsPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+  },*/
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,8 +4,47 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
+    children: [
+        {
+            path: "devices",
+            children: [
+              {
+                path: '',
+                loadChildren: () => import('../devices/devices.module').then( m => m.DevicesPageModule)
+              }
+            ]
+        },
+        {
+          path: "maps",
+          children: [
+            {
+              path: '',
+              loadChildren: () => import('../maps/maps.module').then( m => m.MapsPageModule)
+            }
+          ]
+         },
+         {
+              path: "account",
+              children: [
+                {
+                  path: '',
+                  loadChildren: () => import('../account/account.module').then( m => m.AccountPageModule)
+                }
+            ]
+         },
+        {
+              path: '',
+              redirectTo: '/tabs/maps',
+              pathMatch: 'full'
+        },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/maps',
+    pathMatch: 'full'
   }
 ];
 
